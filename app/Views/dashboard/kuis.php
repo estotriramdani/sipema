8241b1d0de43892e991f0d6d91970219921801fd
Esto Triramdani N <estotriramdani@gmail.com>
2020-11-11 22:23:51 +0700

    coba kuis

--- app/Views/dashboard/kuis.php
+++ app/Views/dashboard/kuis.php
@@ -2,5 +2,29 @@
 
 <?= $this->section('content'); ?>
 
-Ini halaman kuis
+<h1>Kuis {materi}</h1>
+
+<p>Pertanyaan 1</p>
+
+<div class="input-group">
+  <div class="input-group-prepend">
+    <div class="input-group-text">
+      <input type="radio" name="pilihan" id="jawaban1" aria-label="Radio button for following text input">
+    </div>
+  </div>
+  <input type="text" class="form-control" aria-label="Text input with radio button">
+</div>
+<div class="input-group">
+  <div class="input-group-prepend">
+    <div class="input-group-text">
+      <input type="radio" name="pilihan" id="jawaban2" aria-label="Radio button for following text input">
+    </div>
+  </div>
+  <input type="text" class="form-control" aria-label="Text input with radio button">
+</div>
+
+
+<script src="/js/kuis.js"></script>
+
+
 <?= $this->endSection(); ?>
---
+++ public/js/kuis.js
@@ -0,0 +1,10 @@
+localStorage.setItem("no1", "a");
+localStorage.setItem("no2", "a");
+localStorage.setItem("no3", "a");
+localStorage.setItem("no4", "a");
+localStorage.setItem("no5", "a");
+
+
+
+
+// document.getElementById("result").innerHTML = localStorage.getItem("no1");
