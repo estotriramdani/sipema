8fef76cd3a6df9845525f1b60e3bb96581c68ada
estotriramdani <60980963+estotriramdani@users.noreply.github.com>
2020-11-15 13:41:28 +0700

    Update bangundatar.php

--- app/Views/dashboard/materi/bangundatar.php
+++ app/Views/dashboard/materi/bangundatar.php
@@ -2,29 +2,29 @@
 
 
 <div class="reveal">
-    <div class="slides">
-        <section>
+  <div class="slides">
+    <section>
 
-            <h2>Materi #1</h2>
-            <h2 class="r-fit-text">Bangun Datar</h2>
-            <h2>SIPEMA</h2>
-            <small>Jika menggunakan ponsel, mohon gunakan/aktifkan fitur landscape</small>
+      <h2>Materi #1</h2>
+      <h2 class="r-fit-text">Bangun Datar</h2>
+      <h2>SIPEMA</h2>
+      <small>Jika menggunakan ponsel, mohon gunakan/aktifkan fitur landscape</small>
 
-        </section>
-        <section>
-            <section>
+    </section>
+    <section>
+      <section>
 
-                <h2>Vertical 2</h2>
-            </section>
-            <section>2.1</section>
-            <section>2.2</section>
-        </section>
-        <section>Slide 3</section>
-        <section>
-            <a href="#/0">Kembali ke halaman awal</a> <br>
-            <a href="/materi">Kembali ke Materi</a>
-        </section>
-    </div>
+        <h2>Vertical 2</h2>
+      </section>
+      <section>2.1</section>
+      <section>2.2</section>
+    </section>
+    <section>Slide 3</section>
+    <section>
+      <a href="#/0">Kembali ke halaman awal</a> <br>
+      <a href="/materi">Kembali ke Materi</a>
+    </section>
+  </div>
 </div>
 
 <?= $this->include('dashboard/materi/footer'); ?>
